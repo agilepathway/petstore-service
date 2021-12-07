376755aaeefec9da9103df12c70eceb432d1573a
John Boyes <154404+johnboyes@users.noreply.github.com>
2021-12-07 21:48:26 +0000

    Add generated files to source control
    The generated Java server's endpoints all return with a default 501 (not
    implemented) response, currently.  In a future commit we will implement
    the endpoints needed to verify the service's contract with its consumer.

---
+++ src/main/java/uk/co/agilepathway/petstore/api/StoreApiController.java
@@ -0,0 +1,22 @@
+package uk.co.agilepathway.petstore.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Optional;
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2021-12-07T21:40:18.841628Z[Etc/UTC]")
+@Controller
+@RequestMapping("${openapi.swaggerPetstore.base-path:/v2}")
+public class StoreApiController implements StoreApi {
+
+    private final StoreApiDelegate delegate;
+
+    public StoreApiController(@org.springframework.beans.factory.annotation.Autowired(required = false) StoreApiDelegate delegate) {
+        this.delegate = Optional.ofNullable(delegate).orElse(new StoreApiDelegate() {});
+    }
+
+    @Override
+    public StoreApiDelegate getDelegate() {
+        return delegate;
+    }
+
+}
